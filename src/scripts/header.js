71cd40a95f8fa2150d30ba2e5310910c8bfed711
priyankalima <priyankalima99@gmail.com>
2024-07-24 13:03:00 +0530

    commit

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -6,12 +6,12 @@ header.append(
             id: "navbar",
             className: "navbar",
             innerHTML: `
-            <div class="container">
-                <div class="logo">
+            <div class="container d-row-res">
+                <div class="logo d-flex">
                     <img id="logoImg" class="logo-img" alt="logo image">
                     <div id="toggleBtn" class="toggle-btn"></div>
                 </div>
-                <div class="menu">
+                <div class="d-col" id="menu">
                     <div class="cto-btn">
                         <button>Book Free Demo</button>
                         <button>Parent Login</button>
@@ -36,16 +36,43 @@ header.append(
                 <div></div>
                 <div></div>
                 <div></div>`
-                // hide and show the menu when the max width 600px
+                // menu and resmenu when the media is max wifth 600px then dhow resmenu else menu
+                // Create a match Function
+                // Create a match Function
+                function myFunction(x) {
+                    if (x.matches) {
+                        menu.classList.add('res-menu')
+                        menu.classList.remove('menu')
+                    } else {
+                        menu.classList.add('menu')
+                        menu.classList.remove('res-menu')
+                    }
+                }
+                // Create a MediaQueryList object
+                const media = window.matchMedia("(max-width: 500px)")
+                // Call the match function at run time
+                myFunction(media);
+                // Add the match function as a listener for state changes
+                media.addEventListener("change", function () {
+                    myFunction(media);
+                });
+                // by default res menu is none and remove classlist
+                if (menu.classList.contains('res-menu')) {
+                    // remove classlist
+                    menu.classList.remove('res-menu');
+                    menu.style.display = "none";
+                }
+                // hide and show the menu
                 toggleBtn.addEventListener('click', () => {
-                    // if the menulist is none show block 
-                    if (menuList.style.display === "block") { 
-                        menuList.style.display = "none";
-                        console.log('its true')
+                    if (menu.classList.contains('res-menu')) {
+                        // remove classlist
+                        menu.classList.remove('res-menu');
+                        menu.style.display = "none";
                     }
                     else {
-                        menuList.style.display = "block"
-                        console.log('its false')
+                        // add menu classlist
+                        menu.classList.add('res-menu')
+                        menu.style.display = "block";
                     }
                 })
             })
