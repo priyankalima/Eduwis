4fbd6e94d288dd2db15f9820a439d6eaff269f4b
priyankalima <priyankalima99@gmail.com>
2024-07-27 13:18:18 +0530

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,51 @@
-main.appendChild(
+main.append(
     Object.assign(
-        document.createElement('span'),
+        document.createElement('section'),
+        { 
+            innerHTML:`
+            <div class="container d-row-rspn">
+                 <div class="left-content d-col" id="leftContent"></div>
+                 <div class="right-image" id="rightImage"></div>
+            </div>
+            `,
+            function:addEventListener('load',()=>{
+                fetch('./content.json').then(res=>res.json()).then((data)=>{
+                    const item = data.main.sectionOne;
+                    leftContent.innerHTML += `
+                    <span>${item.title}</span>
+                    <span>${item.heading}</span>
+                    <span>${item.para}</span>
+                    `
+                })
+            })
+        } 
+    ),
+    Object.assign(
+        document.createElement('section'),
+        {
+            innerHTML:"this is section two"
+        } 
+    ),
+    Object.assign(
+        document.createElement('section'),
+        {
+            innerHTML:"this is section three"
+        } 
+    ),
+    Object.assign(
+        document.createElement('section'),
+        {
+            innerHTML:"this is section four"
+        } 
+    ),
+    Object.assign(
+        document.createElement('section'),
+        {
+            innerHTML:"this is section five"
+        } 
+    ),
+    Object.assign(
+        document.createElement('section'),
         {
             innerHTML:"this is main page"
         } 
